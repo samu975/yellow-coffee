e6069bde5d5c31757002e3c998745b562d34158c
Samuel Rosero <saferoxx215@gmail.com>
2023-10-02 13:30:57 -0500

    fix the config for client

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
+// import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  output: 'static',
+  // adapter: nodejs(),
+  // adapter: vercel(),
+  // site: 'https://yellowcoffee.com.co',
 });
